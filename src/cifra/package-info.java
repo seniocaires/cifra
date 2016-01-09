d4695d8368417d5f879c527bbd9731ba31532082
Senio Caires <seniocaires@gmail.com>
2016-01-09 01:06:58 -0200

    sonar issues

--- src/cifra/package-info.java
+++ src/cifra/package-info.java
@@ -1,4 +1,25 @@
+/*
+ * Cifra, ferramenta para cifrar textos.
+ * Copyright (c) 2015 Sênio Caires
+ * e-mail: seniocaires@gmail.com
+ * 
+ * Este arquivo é parte do programa Cifra
+ * 
+ * Cifra é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da Licença Pública Geral GNU como
+ * publicada pela Fundação do Software Livre (FSF); na versão 3 da
+ * Licença, ou (na sua opinião) qualquer versão.
+ * 
+ * Este programa é distribuído na esperança de que possa ser  útil,
+ * mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+ * a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+ * Licença Pública Geral GNU para maiores detalhes.
+ * 
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+ * com este programa, Se não, veja <http://www.gnu.org/licenses/>.
+ */
 /**
  * Único pacote da aplicação.
+ * @author Senio Caires
  */
 package cifra;
